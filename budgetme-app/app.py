ef9deb7029bdaf112eebd882795b1471e2eb63d4
heysus-me <vksjesus@gmail.com>
2024-12-06 23:01:51 -0500

    Initial Flask setup

---
+++ budgetme-app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#Create an instance of the Flask class
+app = Flask(__name__)
+
+#Define a route for the home page
+@app.route('/')
+def home():
+    return "Welcome to the budgeting Application!"
+
+#Run the app if this file is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
